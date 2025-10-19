9eaf23a2df7fc06e7ec14802e301a34a2ebbb21f
mdjubuntu <mjnhdi35@gmail.com>
2025-10-19 20:35:03 +0700

    update

--- apps/server/src/users/users.controller.ts
+++ apps/server/src/users/users.controller.ts
@@ -4,11 +4,4 @@ import { UsersService } from './users.service';
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-
-  @Get()
-  async getUsers() {
-    return {
-      data: 'Alice',
-    };
-  }
 }
