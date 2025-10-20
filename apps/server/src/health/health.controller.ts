1c2bf0b9fb8b536bb3e6516cfe89c9e99dc6ca16
mdjubuntu <mjnhdi35@gmail.com>
2025-10-20 22:25:30 +0700

    update

--- apps/server/src/health/health.controller.ts
+++ apps/server/src/health/health.controller.ts
@@ -4,23 +4,44 @@ import {
   HealthCheck,
   TypeOrmHealthIndicator,
 } from '@nestjs/terminus';
+import { Public } from '../core/decorators/public.decorator';
+import { RedisService } from '../redis/redis.service';
 
 @Controller('health')
 export class HealthController {
   constructor(
     private health: HealthCheckService,
     private db: TypeOrmHealthIndicator,
+    private redis: RedisService,
   ) {}
-
-  @Get()
-  @HealthCheck()
-  check() {
-    return this.health.check([() => this.db.pingCheck('database')]);
-  }
 
   @Get('db')
   @HealthCheck()
   checkDatabase() {
     return this.health.check([() => this.db.pingCheck('database')]);
+  }
+
+  @Get('redis')
+  @HealthCheck()
+  checkRedis() {
+    return this.health.check([
+      async () => {
+        const isHealthy = await this.redis.ping();
+        return { redis: { status: isHealthy ? 'up' : 'down' } };
+      },
+    ]);
+  }
+
+  @Public()
+  @Get('all')
+  @HealthCheck()
+  checkAll() {
+    return this.health.check([
+      () => this.db.pingCheck('database'),
+      async () => {
+        const isHealthy = await this.redis.ping();
+        return { redis: { status: isHealthy ? 'up' : 'down' } };
+      },
+    ]);
   }
 }
